d350ddeedd90d9a5b8180846461348291924d634
S M Mohi Us Sunnat <suncha629@gmail.com>
2016-07-20 01:37:24 +0600

    Minor change

--- app/src/main/java/com/dreamogrammers/sunnat/alertdisplay/MainActivity.java
+++ app/src/main/java/com/dreamogrammers/sunnat/alertdisplay/MainActivity.java
@@ -5,13 +5,14 @@ import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends MotherClass implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonSimpleToast, buttonSimpleSnackbar, buttonActionCallback, buttonCustomSnackbar;
     private CoordinatorLayout coordinatorLayout;
